108b57a234ec0a16b3a58ebae59089963235155e
jakob svenningsson <jaksve@kth.se>
2020-03-03 11:56:19 +0100

    Added assigmenet 7.2.2.

---
+++ 7.2.2/app/src/main/java/com/example/a7_2_2/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.a7_2_2
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.util.Log
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun vibrate(_view: View) {
+        val vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        if (vibrator.hasVibrator()) {
+            val vibEffect = VibrationEffect
+                .createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE)
+            vibrator.vibrate(vibEffect)
+        } else {
+            Log.v("vibrate", "no vibrator on device.")
+        }
+    }
+}
